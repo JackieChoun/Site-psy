16661cf22269313a74a364efd83de1c7f528772b
JackieChoun <ydv311@hotmail.fr>
2026-03-02 16:27:34 +0100

    centrage page contact

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}",
+    "./node_modules/preline/dist/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
